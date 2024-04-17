f626ba8a3cbdf0e524c9eaa5bee60ca8beb42a91
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-04-17 14:28:50 +0800

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,16 +1,18 @@
 const _unicodeCharactersListSection = document.getElementById("unicode-characters-list");
 
-fetch("./src/data.json").then(response => response.json()).then(data => {
-    console.log(data);
-    for (let k = 0; 100*k < data.length; k++) {
+async function fetchDataAndRender(startIndex) {
+    await fetch("./src/data.json")
+        .then(response => response.json())
+        .then(data => {
+            const endIndex = Math.min(startIndex + 100, data.length);
 
-        const _header = document.createElement("h3");
-        _header.innerText = `${100*k} - ${100*k+99}`;
-        _unicodeCharactersListSection.appendChild(_header);
+            const _header = document.createElement("h3");
+            _header.innerText = `${startIndex} - ${endIndex - 1}`;
+            _unicodeCharactersListSection.appendChild(_header);
 
-        const _table = document.createElement("table");
-        _table.style.width = "100%";
-        _table.innerHTML = `
+            const _table = document.createElement("table");
+            _table.style.width = "100%";
+            _table.innerHTML = `
             <thead>
                 <tr>
                     <th>ID</th>
@@ -22,32 +24,54 @@ fetch("./src/data.json").then(response => response.json()).then(data => {
                 </tr>
             </thead>
             <tbody></tbody>
-        `;
-
-        const _tbody = _table.querySelector("tbody");
-        for (let i = 0; i < 100; i++) {
-            const item = data[100*k + i];
-            let _tr = document.createElement("tr");
-            _tr.onmouseover = () => {
-                _tr.style.backgroundColor = "#f0f0f0";
-            }
-            _tr.onmouseout = () => {
-                _tr.style.backgroundColor = "transparent";
-            }
-            _tr.className = `char-${100*k + i} char-\\u${(100*k + i).toString(16)}`;
-            _tr.innerHTML = `
-                <td style="text-align: center;">${item.id}</td>
-                <td style="text-align: center;"><code>${item.char}</code></td>
-                <td>${item.name}</td>
-                <td style="text-align: center;">${item.type}</td>
-                <td style="text-align: center;">${item["code point"]}</td>
-                <td style="text-align: center;">${item.width}</td>
             `;
-            _tbody.appendChild(_tr);
-        }
 
-        _unicodeCharactersListSection.appendChild(_table);
-    }
-}).catch(error => {
-    console.error(error);
-});
+            for (let k = startIndex; k < endIndex; k++) {
+                const _tbody = _table.querySelector("tbody");
+                const item = data[k];
+                let _tr = document.createElement("tr");
+                _tr.onmouseover = () => {
+                    _tr.style.backgroundColor = "#f0f0f0";
+                }
+                _tr.onmouseout = () => {
+                    _tr.style.backgroundColor = "transparent";
+                }
+                _tr.className = `char-${k} char-\\u${k.toString(16)}`;
+                _tr.innerHTML = `
+                    <td style="text-align: center;">${item.id}</td>
+                    <td style="text-align: center;"><code>${item.char}</code></td>
+                    <td>${item.name}</td>
+                    <td style="text-align: center;">${item.type}</td>
+                    <td style="text-align: center;">${item["code point"]}</td>
+                    <td style="text-align: center;">${item.width}</td>
+                `;
+                _tbody.appendChild(_tr);
+            }
+            _unicodeCharactersListSection.appendChild(_table);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+function lazyLoadData() {
+    let startIndex = 0;
+    fetchDataAndRender(startIndex);
+
+    let flag = true
+
+    document.addEventListener("scroll", async function () {
+        const lastRow = document.querySelector("footer")
+        const lastRowOffset = lastRow.offsetTop + lastRow.clientHeight;
+        const pageOffset = window.scrollY + window.innerHeight;
+
+        if (pageOffset > lastRowOffset + 10 && flag) {
+            startIndex += 100;
+            flag = false;
+            await fetchDataAndRender(startIndex);
+            flag = true;
+        }
+    });
+}
+
+lazyLoadData();
